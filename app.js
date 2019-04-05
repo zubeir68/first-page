6f9d5b26a31aaf808473263d47a2ed6b7c30e456
zubeir68 <zubeir.mohamed@outlook.de>
2019-04-05 02:26:28 +0200

    Add eslint and scripts

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-let express = require('express');
-let chalk = require('chalk');
-let debug = require('debug')('app');
-let morgan = require('morgan')
-let path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-
-let app = express();
+const app = express();
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist/js')));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/js')));
 
-app.listen(3000, function() {
-    debug(`listening on port ${chalk.green('3000')}`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`);
 });
