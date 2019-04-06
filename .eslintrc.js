e5daf26055235fcb145f307221828f255e9be7b2
zubeir68 <zubeir.mohamed@outlook.de>
2019-04-06 14:44:31 +0200

    Add express router for book, also hardcode data

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
+        "comma-dangle": 0
     }
 };
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -15,8 +16,46 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/js
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const books = [{
+  author: 'Chinua Achebe',
+  title: 'Things Fall Apart',
+  genre: 'Action',
+  read: false
+},
+{
+  author: 'Hans Christian Andersen',
+  title: 'Fairy tales',
+  genre: 'Comedy',
+  read: false
+},
+{
+  author: 'Dante Alighieri',
+  title: 'The Divine Comedy',
+  genre: 'Love',
+  read: false
+}];
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render('books', {
+      title: 'MyLibrary',
+      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+      books
+    });
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { title: 'MyLibrary' });
+  res.render('index',
+    {
+      title: 'MyLibrary',
+      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+    });
 });
 
 app.listen(port, () => {
